733273b77c058a3d077faf8aa0fec8c88b4d99f9
clay-wangzhi <clay.wangzhi@gmail.com>
2021-04-06 16:44:17 +0800

    zabbix(doc): update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -80,7 +80,7 @@ module.exports = config({
           {
             text: "监控工具",
             items: [
-              { text: "ELK", icon: "elastic", link: "/monitor/zabbix/" },
+              { text: "zabbix", icon: "zabbix", link: "/monitor/zabbix/" },
               { text: "ELK", icon: "elastic", link: "/monitor/elk/" },
             ],
           },
