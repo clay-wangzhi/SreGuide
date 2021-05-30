81efb105eacea75870354a9e104fa36eb1d5d5d4
clay-wangzhi <clay.wangzhi@gmail.com>
2021-05-30 16:11:15 +0800

    add golang readme

--- src/.vuepress/config/sideBar/index.js
+++ src/.vuepress/config/sideBar/index.js
@@ -4,6 +4,7 @@ module.exports = {
     "/basic/network/": require("./basic/network"),
     "/code/shell/": require("./code/shell"),
     "/code/python/": require("./code/python"),
+    "/code/golang/": require("./code/golang"),
     "/cloudnative/docker/": require("./cloudnative/docker"),
     "/automate/ansible/": require("./automate/ansible"),
     "/web/nginx/": require("./web/nginx"),
---
+++ src/code/golang/readme.md
@@ -0,0 +1,20 @@
+---
+category: Golang
+---
+
+# 前言
+
+## 常用官网
+
+[golang.google.cn](https://golang.google.cn/) :star:
+
+[go 语言中文网](https://studygolang.com/) :star:
+
+[GoCN社区](https://gocn.vip/)
+
+
+
+## Go 语言教程
+
+[Go 语言圣经（中文版）](http://books.studygolang.com/gopl-zh/) :star:
+
