6b88b95948e440287daa4e1503578cb7c837c39f
clay-wangzhi <clay.wangzhi@gmail.com>
2021-06-03 15:13:07 +0800

    docs: update jenkins

--- src/.vuepress/config/sideBar/automate/jenkins.js
+++ src/.vuepress/config/sideBar/automate/jenkins.js
@@ -18,7 +18,7 @@ module.exports = [
   {
     title: "4 流水线核心语法",
     collapsable: false,
-    children: ["start-jenkinsfile", "pipelinesyntax", "groovy-simple-tutorial", "jenkins-shared-library", "dingding-plugin", "pipeline-dev-tools"]
+    children: ["start-jenkinsfile", "pipelinesyntax", "groovy-simple-tutorial", "jenkins-shared-library", "dingding-plugin", "pipeline-dev-tools", "pipeline-example"]
   },
   {
     title: "5 流水线集成篇",
